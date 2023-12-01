8bd1279299f17016d947861e997b6b89e9bc2951
KodeJaROM <138987362+KodeJaROM@users.noreply.github.com>
2023-11-30 20:07:06 -0600

    v1.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.kodejarom.notes"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 10
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
